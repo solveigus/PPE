53722fc3b3b910de2edc8af2ec938fa6c7a04948
solveigus <solveig.berling@edu.ece.fr>
2024-04-10 14:25:20 +0200

    fix: bdd insertion

--- app/pages/quizz.js
+++ app/pages/quizz.js
@@ -222,84 +222,10 @@ export default function Page() {
     if (selectedOption) {
       const nextQuestion = currentQuestionObj.options.find(opt => opt.id === selectedOption).nextQuestion;
 
-      const mobilityId = 4;//TODO: identification
+      const mobilityId = 2;//TODO: identification
       
-      /*if (currentQuestionObj.id ===  2) { //MAIN VALIDE
-        if(selectedOption === "ConsolePC"){
-            const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "PC"}]);//creation mobility console
-            const handData = await supabase.from('hand').insert([{ hand_valid: true, id_mobility: mobilityId }]);//creation de la main
-            if (error) {
-              console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-            }
-        }else if(selectedOption === "ConsolePS3"){
-          const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "PS3"}]);
-          const handData = await supabase.from('hand').insert([{ hand_valid: true, id_mobility: mobilityId }]);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-          }
-        }else if(selectedOption === "ConsolePS4"){
-          const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "PS4"}]);
-          const handData = await supabase.from('hand').insert([{ hand_valid: true, id_mobility: mobilityId }]);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-          }
-        }else if(selectedOption === "ConsolePS5"){
-          const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "PS5"}]);
-          const handData = await supabase.from('hand').insert([{ hand_valid: true, id_mobility: mobilityId }]);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-          }
-        }else if(selectedOption === "ConsoleXbox"){
-          const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "Xbox"}]);
-          const handData = await supabase.from('hand').insert([{ hand_valid: true, id_mobility: mobilityId }]);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-          }
-        }else if(selectedOption === "ConsoleSwitch"){
-          const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "Switch"}]);
-          const handData = await supabase.from('hand').insert([{ hand_valid: true, id_mobility: mobilityId }]);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-          }
-        }
-      }*/
 
-
-      /*if (currentQuestionObj.id ===  3) { //MAIN PAS VALIDE
-        if(selectedOption === "ConsolePC"){
-            const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "PC"}]);
-            if (error) {
-              console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-            }
-        }else if(selectedOption === "ConsolePS3"){
-          const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "PS3"}]);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-          }
-        }else if(selectedOption === "ConsolePS4"){
-          const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "PS4"}]);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-          }
-        }else if(selectedOption === "ConsolePS5"){
-          const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "PS5"}]);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-          }
-        }else if(selectedOption === "ConsoleXbox"){
-          const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "Xbox"}]);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-          }
-        }else if(selectedOption === "ConsoleSwitch"){
-          const user = await supabase.from('mobility').insert([{ id: mobilityId , console: "Switch"}]);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-          }
-        }
-      }*/
-
-      if (currentQuestionObj.id === 2) { // MAIN VALIDE
+      if (currentQuestionObj.id === 3) { // MAIN PAS VALIDE
         try {
           let consoleType = "";
           switch(selectedOption) {
@@ -324,53 +250,88 @@ export default function Page() {
             default:
               console.error("Option invalide sélectionnée.");
           }
-  
-          // Créez une instance dans la table "mobility" avec l'ID aléatoire et le type de console sélectionné
-          const mobilityData = await supabase.from('mobility').insert([{ id: mobilityId, console: consoleType }]);
-          
-          // Créez une instance dans la table "hand" avec hand_valid à true et l'ID de mobilité correspondant
-          const handData = await supabase.from('hand').insert([{id: 8, hand_valid: true, id_mobility: mobilityId }]);
-          
-          if (mobilityData.error || handData.error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', mobilityData.error || handData.error);
-          } else {
-            console.log('Instances créées avec succès:', mobilityData.data, handData.data);
-          }
+            
+          const { data, error } = await supabase
+          .from('hand')
+          .insert([
+            { hand_valid: false, id_mobility: mobilityId },
+          ])
+          .select()
+
         } catch (error) {
           console.error('Erreur lors de la mise à jour de la base de données:', error.message);
         }
       }
-  
 
 
-      /*if (currentQuestionObj.id === 4 ) { //JOYSTICKS
-        if(selectedOption === "MainFermee"){
-            const { data, error } = await supabase.from('hand').update({ hand_closed: true }).eq('id', mobilityId);// Mettre à jour valeur dans la table
-            if (error) {
-              console.error('Erreur lors de la mise à jour de la base de données:', error.message);
-            }
-        }else if(selectedOption === "MainMiouverte"){
-          const { data, error } = await supabase.from('hand').update({ hand_half_open: true }).eq('id', mobilityId);
-          if (error) {
-            console.error('Erreur lors de la mise à jour de la base de données:', error.message);
+      if (currentQuestionObj.id === 4) { // MAIN PAS VALIDE
+        try {
+          let consoleType = "";
+          switch(selectedOption) {
+            case "MainOuverte":
+              const { data, error } = await supabase
+              .from('hand')
+              .update({ hand_open: true })
+              .eq('id', mobilityId )
+              .select()
+              break;
+            case "MainMiouverte":
+              const { data1, error1 } = await supabase
+              .from('hand')
+              .update({ hand_half_open: true })
+              .eq('id', mobilityId )
+              .select()
+              break;
+            case "MainFermee":
+              const { data2, error2 } = await supabase
+              .from('hand')
+              .update({ hand_closed: true })
+              .eq('id', mobilityId )
+              .select()
+              break;
+            default:
+              console.error("Option invalide sélectionnée.");
           }
-        }else if(selectedOption === "MainOuverte"){
-          const { data, error } = await supabase.from('hand').update({ hand_open: true }).eq('id', mobilityId);
-          if (error) {
+          } catch (error) {
             console.error('Erreur lors de la mise à jour de la base de données:', error.message);
           }
         }
-      }*/
 
-      /*if (currentQuestionObj.id === 4 ) {
-        if(selectedOption === "MainFermee"){
-            // Mettre à jour la valeur dans la table "hand" où id = 1 (par exemple)
-            const { data, error } = await supabase.from('hand').update({ hand_closed: false }).eq('id', 1);
-            if (error) {
+        if (currentQuestionObj.id === 5) { // MAIN PAS VALIDE
+          try {
+            let consoleType = "";
+            switch(selectedOption) {
+              case "MainOuverte":
+                const { data, error } = await supabase
+                .from('hand')
+                .update({ hand_open: true })
+                .eq('id', mobilityId )
+                .select()
+                break;
+              case "MainMiouverte":
+                const { data1, error1 } = await supabase
+                .from('hand')
+                .update({ hand_half_open: true })
+                .eq('id', mobilityId )
+                .select()
+                break;
+              case "MainFermee":
+                const { data2, error2 } = await supabase
+                .from('hand')
+                .update({ hand_closed: true })
+                .eq('id', mobilityId )
+                .select()
+                break;
+              default:
+                console.error("Option invalide sélectionnée.");
+            }
+            } catch (error) {
               console.error('Erreur lors de la mise à jour de la base de données:', error.message);
             }
-        }
-      }*/
+          }
+
+
+
 
       setCurrentQuestion(nextQuestion);
       setSelectedOption(null);
